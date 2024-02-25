2d67b1006a5b81157aa8467bfa6782a33f32ec54
Peppi Tarkka <peppi.tarkka@aalto.fi>
2024-02-25 14:22:19 +0200

    Tiedostoja ja metodeja hahmoteltu, mutta ei toteutettu

---
+++ src/main/scala/App.scala
@@ -0,0 +1,11 @@
+class App(furnitures: Array[Furniture]):
+
+  def onClick: Unit = ???
+
+  def onMouseMove: Unit = ???
+
+  def downLoadPic: Unit = ???
+
+  def rotateFurniture(f: furniture): Unit = ???
+
+  def save() = ???
