2aeab70f7a6af29ea410b592f216d056a738a328
Peppi Tarkka <peppi.tarkka@aalto.fi>
2024-03-17 14:40:40 +0200

    väli push :3

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -9,6 +9,7 @@ import scalafx.scene.paint.Color.*
 import scalafx.scene.text.Font
 import scalafx.stage.FileChooser
 import scalafx.stage.FileChooser.ExtensionFilter
+import java.io.File
 
 object Main extends JFXApp3:
 
@@ -22,36 +23,45 @@ object Main extends JFXApp3:
 
     val root = GridPane()
 
+    val fileNew = MenuItem("New")
+      fileNew.onAction = (event) =>
+
+        val filechooser = new FileChooser:
+         title = "Select the picture of the floorplan"
+      //Opens the files
+         initialDirectory = File("./src/main/DreamSpace.testikansio")  //MIKS EI LÖYDÄ?
+         extensionFilters.add(ExtensionFilter("PNG and JPG", Seq("*.png", "*.jpg"))) //WHAAAT???
+
+        val file = filechooser.showOpenDialog(stage)
+
+
     //Here is the button for filechooser don't know how to combine the filechooserbutton to this
     val menu = new Menu("File"):
-      items = Array(MenuItem("New"), MenuItem("Save"))
+      items = Array(fileNew, MenuItem("Save"))
 
     val top = new MenuBar:
       menus = Array(menu)
 
     //NÄÄ 2 KOMMATAAN NII OHJELMA TOIMII
-    /*
-    val filechooser = new FileChooser:
-      title = "Select the picture of the floorplan"
-      //Opens the files
-      initialDirectory = File("./src/main/DreamSpace.testikansio")  //MIKS EI LÖYDÄ?
-      extensionFilters.add(ExtensionFilter("PNG", "*.png", "JPG", ".jpg")) //WHAAAT???
 
-    val file = filechooser.showOpenDialog(stage)
-    */
 
     // Buttons for shapes
-    val circleB = new ButtonType("Circle")
+    /*val circleB = new ButtonType("Circle")
     val rectangleB = new ButtonType("Rectangle")
     val ellipseB = new ButtonType("Ellipse")
-    val halfRB = new ButtonType("Half Round")
+    val halfRB = new ButtonType("Half Round")*/
 
 
     // Buttons for furnitures:
     val tableButton = new Button("Table")
       tableButton.onAction = (event) =>
 
-        var selectShape = new Alert(AlertType.Confirmation):
+        val circleB = new ButtonType("Circle")
+        val rectangleB = new ButtonType("Rectangle")
+        val ellipseB = new ButtonType("Ellipse")
+        val halfRB = new ButtonType("Half Round")
+
+        val selectShape = new Alert(AlertType.Confirmation):
           initOwner(stage)
           headerText = "First select shape!"
           contentText = "Choose your option."
@@ -60,6 +70,7 @@ object Main extends JFXApp3:
         val result = selectShape.showAndWait()
 
         result match
+          case Some(rectangleB) => println("lslslslsl")
           case Some(circleB) => println("you chose circle")
           case _ => println("either cancel or closed the whole thing")   //BUGI!!!!!!
 
